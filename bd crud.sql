211c759d9182ecb99c1d2e1947e1eafbd17418cf
Juan David Ruiz <65297648+Juan1998-Cod@users.noreply.github.com>
2023-12-05 16:10:24 -0500

    Bases de datos

---
+++ bd crud.sql
@@ -0,0 +1,38 @@
+- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema crudoperation
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema crudoperation
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `crudoperation` DEFAULT CHARACTER SET utf8mb4 ;
+USE `crudoperation` ;
+
+-- -----------------------------------------------------
+-- Table `crudoperation`.`crud`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `crudoperation`.`crud` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `apellido` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `mobile` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
